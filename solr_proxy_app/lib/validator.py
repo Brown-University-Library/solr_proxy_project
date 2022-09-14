e79b12717cf9b655662f7a65513e049a91fb5371
Birkin James Diana <birkin.diana@gmail.com>
2022-09-14 10:10:48 -0400

    stubs called function.

--- solr_proxy_app/lib/validator.py
+++ solr_proxy_app/lib/validator.py
@@ -5,6 +5,7 @@ log = logging.getLogger(__name__)
 
 
 def check_core( core: str ) -> tuple[ bool, bool]:
+    """ Checks the core-code. """
     err: bool = True
     validity: bool = False
     log.debug( f'err, ``{err}``; validity, ``{validity}``' )
@@ -14,3 +15,11 @@ def check_core( core: str ) -> tuple[ bool, bool]:
             key_a, key_b,
         ]
     }
+
+
+def get_parts( url: str ) -> dict[ str, str ]:
+    """ Separates the root url and the param-string. """
+    temp_return = {
+        'main': 'foo', 'param_string': 'bar'
+    }
+    return temp_return
